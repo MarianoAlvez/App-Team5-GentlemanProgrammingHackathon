c71cfd48ccb7d53aebc9414880c7ab457e8ea0fd
Mariano Alvez <alvez_jmariano@hotmail.com>
2021-09-26 02:25:32 -0300

    Pagina Login & Registration Listas| con estilos |  validaciones | Responsive ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
+import { ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './shared/login/login.component';
+import { RegistrationComponent } from './shared/registration/registration.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegistrationComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/shared/login/login.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, NgModel } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+  login = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    contrasena: new FormControl('', [Validators.required, Validators.minLength(5)])
+  })
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  get email() {
+    return this.login.get('email')
+  }
+  get contrasena() {
+    return this.login.get('contrasena')
+  }
+
+}
