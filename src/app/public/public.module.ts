61f979e13b240c7c0ce58762474af4d4128a4e60
Mariano Alvez <alvez_jmariano@hotmail.com>
2021-09-26 16:45:31 -0300

    Creacion Modulo passwor_reset

--- src/app/public/public.module.ts
+++ src/app/public/public.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { Password_resetComponent } from './password_reset/password_reset.component';
 import { RegistrationComponent } from './registration/registration.component';
 import { LoginComponent } from './login/login.component';
 
@@ -7,7 +9,7 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { PublicRoutingModule } from './public.routing.module';
 
 @NgModule({
-  declarations: [LoginComponent, RegistrationComponent],
+  declarations: [LoginComponent, RegistrationComponent, Password_resetComponent ],
   imports: [CommonModule, ReactiveFormsModule, PublicRoutingModule],
 })
 export class PublicModule {}
--- src/app/public/public.routing.module.ts
+++ src/app/public/public.routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { Password_resetComponent } from './password_reset/password_reset.component';
 import { RegistrationComponent } from './registration/registration.component';
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'registro', component: RegistrationComponent },
+  { path: 'recuperar', component: Password_resetComponent },
 ];
 @NgModule({
   imports: [RouterModule.forChild(routes)],
