0fb146a9a0630e2c33bab282b30c3b23a5b71c2a
Mariano Alvez <alvez_jmariano@hotmail.com>
2021-09-27 17:28:11 -0300

    Cambios rutas

--- src/app/public/public.routing.module.ts
+++ src/app/public/public.routing.module.ts
@@ -5,14 +5,20 @@ import { LoginComponent } from './login/login.component';
 import { Password_resetComponent } from './password_reset/password_reset.component';
 import { RegistrationComponent } from './registration/registration.component';
 
+import { ContactoComponent } from '../shared/contacto/contacto.component';
+import { LegalesComponent} from '../shared/legales/legales.component';
+import { PreguntasFrecuentesComponent } from '../shared/preguntas-frecuentes/preguntas-frecuentes.component';
+import { QuienesSomosComponent } from '../shared/quienes-somos/quienes-somos.component';
+
 const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'registro', component: RegistrationComponent },
   { path: 'recuperar', component: Password_resetComponent },
-  { path: 'registro/recuperar', component: Password_resetComponent },
-  { path: '*/registro', component: RegistrationComponent },
-  { path: 'registro/recuperar/login', component: LoginComponent },
-  { path: '*/recuperar', component: Password_resetComponent },  
+
+  { path: 'contacto', component: ContactoComponent },
+  { path: 'preguntasfrecuentes', component: PreguntasFrecuentesComponent },
+  { path: 'legales', component: LegalesComponent },
+  { path: 'quienessomos', component: QuienesSomosComponent },  
 ];
 
 @NgModule({
